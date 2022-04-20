4b92694102e0565cd27564ed266a9d727d63232a
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-04-20 19:55:56 +0600

    s

--- lib/providers/category.dart
+++ lib/providers/category.dart
@@ -4,16 +4,12 @@ import 'package:shop_app2/providers/product.dart';
 
 class Category with ChangeNotifier {
   List<Product> _items = [];
-  void update(List<Product> items) {
-    _items = [...items];
-  }
+  void update(List<Product> items) => _items = [...items];
 
   List<Product> _categoryList = [];
   List<Product> get categoryList => [..._categoryList];
 
-  void setCategoryList() {
-    _categoryList = [..._items];
-  }
+  void setCategoryList() => _categoryList = [..._items];
 
   void updateCategoryList(int i) {
     if (i == 0) {
