7dff4683d8a5cc58ed8016901926af601fbcc795
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-05-01 00:44:56 +0600

    Update constants_2.dart

--- lib/constants/constants_2.dart
+++ lib/constants/constants_2.dart
@@ -4,16 +4,16 @@ import 'package:google_fonts/google_fonts.dart';
 
 const String welcomeText2 =
     'There are vast variety collections of products of top Brands & Categories. As bangaldesh\'s online shopping landscape is expanding every year. ';
-const String welcomeText3 = 'Welcome to\n Hello Bazar !';
+const String welcomeText3 = 'Welcome to\n"Hello Bazar"';
 
 Widget welcomeText1() => RichText(
       text: TextSpan(
         children: [
-          _spanText('Welcome to the world\'s most ', FontWeight.normal, 25,
+          _spanText('Welcome to the world\'s most ', FontWeight.normal, 35,
               Colors.white),
-          _spanText('imaginative ', FontWeight.bold, 25,
+          _spanText('imaginative ', FontWeight.bold, 35,
               const Color.fromARGB(255, 5, 18, 43)),
-          _spanText('marketPlace.\n', FontWeight.normal, 25, Colors.white),
+          _spanText('marketPlace.\n', FontWeight.normal, 35, Colors.white),
           _spanText(
             'looking for fresh products ? Try Hello Bazar. Everything you want find in a single app.',
             FontWeight.normal,
