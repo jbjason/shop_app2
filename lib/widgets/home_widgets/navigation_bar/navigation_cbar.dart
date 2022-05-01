4af2a5fab219a052a03586f10d8a1262365eb70d
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-05-01 16:16:56 +0600

    s

--- lib/widgets/home_widgets/navigation_bar/navigation_cbar.dart
+++ lib/widgets/home_widgets/navigation_bar/navigation_cbar.dart
@@ -10,31 +10,36 @@ class NavigationCBar extends StatelessWidget {
     return CustomPaint(
       painter: NavClipper(),
       child: SizedBox(
-        height: kToolbarHeight,
+        height: kToolbarHeight +5,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Icon(
-                  CupertinoIcons.chat_bubble,
-                  color: Colors.black87,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(7),
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.black45),
+                  child: const Icon(CupertinoIcons.chat_bubble,
+                      color: Colors.white),
                 ),
-                Text('Message   ')
+                const Text('Message')
               ],
             ),
-            Column(),
             Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Icon(
-                  CupertinoIcons.square_split_2x2_fill,
-                  color: Colors.black87,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.black54),
+                  child: const Icon(CupertinoIcons.square_split_2x2_fill,
+                      color: Colors.white),
                 ),
-                Text('    Favorite')
+                const Text('Favorite')
               ],
             ),
           ],
