fcc8adb6245f328d157c9d4c944101dfe4573076
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-05-02 14:46:31 +0600

    Update navigation_cbar.dart

--- lib/widgets/home_widgets/navigation_bar/navigation_cbar.dart
+++ lib/widgets/home_widgets/navigation_bar/navigation_cbar.dart
@@ -7,47 +7,49 @@ class NavigationCBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: NavClipper(),
-      child: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(7),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white.withOpacity(0.3)),
-                  child: const Icon(CupertinoIcons.square_split_2x2_fill),
-                ),
-                const Text('All')
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(7),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white.withOpacity(0.3)),
-                  child: const Icon(CupertinoIcons.heart),
-                ),
-                const Text('Favorites')
-              ],
-            ),
-          ],
+    return Container(
+      margin: const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+      child: CustomPaint(
+        painter: NavClipper(),
+        child: Container(
+          height: kToolbarHeight + 7,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(width: 3, color: Colors.white)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(7),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white.withOpacity(0.7)),
+                    child: const Icon(CupertinoIcons.square_split_2x2_fill),
+                  ),
+                  const Text('All')
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(7),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white.withOpacity(0.7)),
+                    child: const Icon(CupertinoIcons.heart),
+                  ),
+                  const Text('Favorites')
+                ],
+              ),
+            ],
+          ),
         ),
-        margin: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
-        height: kToolbarHeight + 20,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(width: 2, color: Colors.white60)),
       ),
     );
   }
