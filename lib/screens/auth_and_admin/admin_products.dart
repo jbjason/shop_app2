0821d6b3ac260290b35d399313bd87612dd40d3e
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-05-04 04:31:15 +0600

    Update admin_products.dart

--- lib/screens/auth_and_admin/admin_products.dart
+++ lib/screens/auth_and_admin/admin_products.dart
@@ -4,24 +4,27 @@ import 'package:shop_app2/providers/products.dart';
 import 'package:shop_app2/widgets/admin_widgets/admin_product_item.dart';
 
 class AdminProductsScreen extends StatelessWidget {
-  const AdminProductsScreen({ Key? key }) : super(key: key);
+  const AdminProductsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(padding: const EdgeInsets.all(20),
-      child: Consumer<Products>(
-                      builder: (ctx, productData, _) => ListView.builder(
-                          itemBuilder: (_, i) => Column(children: [
-                            AdminProductItem(
-                              id:productData.items[i].id,
-                             title: productData.items[i].title,
-                             imageUrl: productData.items[i].imageUrl,
-                            ),
-                          ]),
-                          itemCount: productData.items.length,
-                        ),
-                      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Consumer<Products>(
+          builder: (ctx, productData, _) => ListView.builder(
+            itemBuilder: (_, i) => Column(
+              children: [
+                AdminProductItem(
+                  id: productData.items[i].id,
+                  title: productData.items[i].title,
+                  imageUrl: productData.items[i].imageUrl,
+                ),
+              ],
+            ),
+            itemCount: productData.items.length,
+          ),
+        ),
       ),
     );
   }
