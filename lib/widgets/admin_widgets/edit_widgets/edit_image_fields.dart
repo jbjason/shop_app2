6568d356dc979b03e079abee135322e6cd009396
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-05-07 16:31:46 +0600

    Update edit_image_fields.dart

--- lib/widgets/admin_widgets/edit_widgets/edit_image_fields.dart
+++ lib/widgets/admin_widgets/edit_widgets/edit_image_fields.dart
@@ -17,68 +17,71 @@ class EditImageFields extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-              height: 100,
-              width: 100,
-              margin: const EdgeInsets.only(top: 8, right: 10),
-              decoration: BoxDecoration(
-                border: Border.all(width: 2, color: AppColors.accent),
-              ),
-              child: controller.text.isEmpty
-                  ? const Center(child: Text('Enter a URL'))
-                  : Image.network(controller.text.trim(), fit: BoxFit.cover),
-            ),
-            Expanded(
-              child: TextFormField(
-                decoration: const InputDecoration(labelText: 'Image Url'),
-                keyboardType: TextInputType.url,
-                controller: controller,
-                textInputAction: TextInputAction.done,
-                validator: (value) {
-                  if (value!.isEmpty && imagesList.isEmpty) {
-                    return 'Please enter a Url';
-                  } else if ((!value.startsWith('http') &&
-                      !value.startsWith('https'))) {
-                    return 'Please enter a valid Url';
-                  }
-                  return null;
-                },
-                onFieldSubmitted: (value) {
-                  if (value.isNotEmpty ||
-                      value.startsWith('http') ||
-                      value.startsWith('https')) {
-                    addImage(value);
-                  }
-                },
-              ),
-            ),
-          ],
-        ),
+        _imageTextField(),
         const SizedBox(height: 20),
-        SizedBox(
-          height: 50,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: imagesList.length,
-            itemBuilder: (context, index) => InkWell(
-              onTap: () => deleteImage(imagesList[index]),
-              child: Container(
-                margin: const EdgeInsets.only(right: 10),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(imagesList[index]),
-                      fit: BoxFit.cover),
-                ),
-                alignment: Alignment.topRight,
-                child: const Icon(Icons.delete_sharp, size: 8),
-              ),
-            ),
-          ),
-        ),
+        _chosenList(),
       ],
     );
   }
+
+  Widget _imageTextField() => Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            margin: const EdgeInsets.only(top: 8, right: 10),
+            decoration: BoxDecoration(
+              border: Border.all(width: 2, color: AppColors.accent),
+            ),
+            child: controller.text.isEmpty
+                ? const Center(child: Text('Enter a URL'))
+                : Image.network(controller.text.trim(), fit: BoxFit.cover),
+          ),
+          Expanded(
+            child: TextFormField(
+              decoration: const InputDecoration(labelText: 'Image Url'),
+              keyboardType: TextInputType.url,
+              controller: controller,
+              textInputAction: TextInputAction.done,
+              validator: (value) {
+                if (value!.isEmpty && imagesList.isEmpty) {
+                  return 'Please enter a Url';
+                } else if ((!value.startsWith('http') &&
+                    !value.startsWith('https'))) {
+                  return 'Please enter a valid Url';
+                }
+                return null;
+              },
+              onFieldSubmitted: (value) {
+                if (value.isNotEmpty ||
+                    value.startsWith('http') ||
+                    value.startsWith('https')) {
+                  addImage(value);
+                }
+              },
+            ),
+          ),
+        ],
+      );
+
+  Widget _chosenList() => SizedBox(
+        height: 50,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: imagesList.length,
+          itemBuilder: (context, index) => InkWell(
+            onTap: () => deleteImage(imagesList[index]),
+            child: Container(
+              margin: const EdgeInsets.only(right: 10),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(imagesList[index]), fit: BoxFit.cover),
+              ),
+              alignment: Alignment.topRight,
+              child: const Icon(Icons.delete_sharp, size: 8),
+            ),
+          ),
+        ),
+      );
 }
